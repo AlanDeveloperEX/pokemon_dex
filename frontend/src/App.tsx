29af977ab1533596f20600f9e6981e1dccdd5a26
Alan Santos <alan.santos.ex.contato@gmail.com>
2022-04-06 02:11:45 -0300

    created react app, custom setup and added nes.css

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <section className="nes-container is-dark member-card">
+        <h1>Hello Wolrd!!!</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
